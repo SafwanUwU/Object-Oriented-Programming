0c2ff02590d3476a87f89ad257a9ef5c7360864a
Safwan Ahmad <safwanahmad787@gmail.com>
2024-10-04 10:22:19 +0600

    first commit

---
+++ Student.py
@@ -0,0 +1,13 @@
+class Student:
+
+    grade = 7
+    print("I am a student of ", grade)
+
+
+safwan = Student()
+afnan = Student()
+sajid = Student()
+
+print(f"Safwans grade : {safwan.grade}")
+print(f"Afnans grade : {afnan.grade}")
+print(f"Sajids grade : {sajid.grade}")
